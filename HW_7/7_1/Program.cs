612157c79da46cc6e4d10d574bcdda2645b32956
Irina Ostapchuk <2376706@gmail.com>
2024-02-12 17:17:47 +0300

    HW_7_1

---
+++ HW_7/7_1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 1 - Задайте значения M и N. Напишите программу, которая выведет
+//все натуральные числа в промежутке от M до N. Использовать рекурсию, не
+//использовать циклы.
+
+
+Console.WriteLine("ВВедение символов от M до N: ");
+
+int m = int.Parse(Console.ReadLine()!);
+int n = int.Parse(Console.ReadLine()!);
+
+PrintNumbersInRange(m, n);
+
+static void PrintNumbersInRange(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.WriteLine(m);
+        m++;
+        PrintNumbersInRange(m, n);
+    }
+}
+
