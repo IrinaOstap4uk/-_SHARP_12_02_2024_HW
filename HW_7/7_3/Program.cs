ab5624f4d00dd2638793191fac10ad81b0f33a73
Irina Ostapchuk <2376706@gmail.com>
2024-02-12 18:25:26 +0300

    HW_7 task 7_3

---
+++ HW_7/7_3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 3 - Задайте произвольный массив. Выведете его
+//элементы, начиная с конца. Использовать рекурсию, не
+//использовать циклы.
+
+void array ()
+{
+    int[] array = { 5, 6, 7, 8, 9, 10 };
+    PrintArrayReversed(array, array.Length - 1);
+}
+
+void PrintArrayReversed(int[] arr, int index)
+{
+    if (index < 0)
+    {
+        return;
+    }
+    else
+    {
+        PrintArrayReversed(arr, index - 1);
+        Console.WriteLine(arr[index]);
+    }
+}
